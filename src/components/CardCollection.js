ca37ac43d3bfadf2efafadeddc67e8b7b70fc053
David Stiven Burgos Peralta <dburgos@consensussa.com>
2022-12-15 10:44:02 -0500

    modify the URL

--- src/components/CardCollection.js
+++ src/components/CardCollection.js
@@ -1,3 +1,4 @@
+import { Button } from 'bootstrap';
 import React, {useState, useEffect} from 'react';
 import PersonalCard from '../components/PersonalCard';
 import './CardCollection.css';
@@ -6,9 +7,16 @@ const CardCollection = () => {
 
     const [charList, setCharList] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [url, setUrl] = useState('https://swapi.dev/api/people/?page=');
+    const [page, setPage] = useState(1);
+
+    const nextPage = () =>{
+        setPage(page + 1);
+        
+    }
 
     useEffect( ()=>{
-        fetch('https://swapi.dev/api/people/')
+        fetch(url + page)
         .then((res) => res.json())
         .then((character) => setCharList(character))
         .catch(err => console.log(err))
@@ -17,17 +25,28 @@ const CardCollection = () => {
 
     return ( 
         <>
-            <input className='search' type= 'search'/>
+            {loaded === false ?(
+                <br></br>
+            ):(
+                <input className='search' type= 'search'/>
+            )}
             <div className='fully'>
-                {console.log(charList)}
+                
                 {loaded === false ?(
-                    <>
-                        <p>lel</p>
-                    </>
+                    <p>Cargando...</p>
                 ):(
                     charList.results.map((el) => <PersonalCard character={el}/>)
                 )}
             </div>
+            {loaded === false ?(
+                <br></br>
+            ):(
+                <div className='bottom'>
+                    <button className='nextButton' > {"<<"} </button>
+                    <button className='nextButton' onClick={nextPage}> {">>"} </button>
+                </div>
+                
+            )}
         </>
      );
 }
